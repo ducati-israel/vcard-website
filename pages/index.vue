1f9a2066d7b0ce2736dbfb2aa14d85fedbffded1
jossef <jossef12@gmail.com>
2022-10-16 12:08:35 +0300

    fix: renamed field

--- pages/index.vue
+++ pages/index.vue
@@ -128,7 +128,7 @@
 
               <div class="app__vcard__info__field">
                 <div class="app__vcard__info__label">
-                  חברות מועדון (בודד\בזוג)
+                  חברות מועדון (יחיד\בזוג)
                 </div>
 
                 <div class="app__vcard__info__value">
